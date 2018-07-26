b24d3276cdac5c784b02cc69de141edddf589928
EvgeniiZaets <evgeniizaets@gmail.com>
2018-07-26 20:44:48 +0300

    Refactoring StaffService

--- services/StaffService.php
+++ services/StaffService.php
@@ -14,54 +14,70 @@ class   StaffService
         // 1. мы испльзуем ActiveREcord, а он не будет работать с конструкторами.
         // 2. может быть несколько способов создания одного и того же обьекта, тогда одного конструктора не хватит.
         $interview = Interview::join($lastName, $firstName, $email, $date);
-        $interview->save(false); // Не запускаем валдацию т.к. данные ужо провалидированы с помощью InteviewJoinForm.
+        $this->save($interview);
 
-        if ($interview->email) {
-            Yii::$app->mailer->compose()
-                ->setFrom(Yii::$app->params['adminEmail'])
-                ->setTo($interview->email)
-                ->setSubject('You are joined to interview!')
-                ->send();
-        }
-
-        $log = new Log();
-        $log->message = $interview->last_name . ' ' . $interview->first_name . ' is joined to interview';
-        $log->save();
+        $this->notify($interview->email, 'You are joined to interview!');
+        $this->log($interview->last_name . ' ' . $interview->first_name . ' is joined to interview');
 
         return $interview;
     }
 
     public function editInterview($id, $lastName, $firstName, $email)
     {
-        if (!$interview = Interview::findOne($id))
-            throw new \DomainException('Interview not found');
-
+        $interview = $this->findInterviewModel($id);
         $interview->editData($lastName, $firstName, $email);
-        $interview->save(false);
+        $this->save($interview);
 
-        $log = new Log();
-        $log->message = 'Interview' . $interview->id . ' is updated';
-        $log->save();
+        $this->log('Interview' . $interview->id . ' is updated');
     }
 
     public function rejectInterview($id, $reason)
     {
+        $interview = $this->findInterviewModel($id);
+        $interview->reject($reason);
+        $this->save($interview);
+
+        $this->notify($interview->email, 'You are failed an interview');
+        $this->log($interview->last_name . ' ' . $interview->first_name . ' is failed an interview');
+    }
+
+    /**
+     * @param $id
+     * @return Interview
+     */
+    private function findInterviewModel($id)
+    {
         if (!$interview = Interview::findOne($id))
             throw new \DomainException('Interview not found');
 
-        $interview->reject($reason);
-        $interview->save(false);
+        return $interview;
+    }
 
-        if ($interview->email) {
-            Yii::$app->mailer->compose()
-                ->setFrom(Yii::$app->params['adminEmail'])
-                ->setTo($interview->email)
-                ->setSubject('You are failed an interview')
-                ->send();
-        }
-
+    private function log($message)
+    {
         $log = new Log();
-        $log->message = $interview->last_name . ' ' . $interview->first_name . ' is failed an interview';
+        $log->message = $message;
         $log->save();
+    }
+
+    private function notify($email, $message)
+    {
+        if ($email) {
+                Yii::$app->mailer->compose()
+                    ->setFrom(Yii::$app->params['adminEmail'])
+                    ->setTo($email)
+                    ->setSubject($message)
+                    ->send();
+        }
+    }
+
+    /**
+     * @param $interview
+     */
+    private function save(Interview $interview)
+    {
+        // Не запускаем валдацию т.к. данные ужо провалидированы с помощью InteviewJoinForm.
+        if (!$interview->save(false))
+            throw new \RuntimeException('Saving error.');
     }
 }
