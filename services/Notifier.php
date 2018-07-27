d846f5ce54ca6e61dfbafe97e95ed5e5bcad3aeb
EvgeniiZaets <evgeniizaets@gmail.com>
2018-07-27 21:38:03 +0300

    Delegate Logger and Notifier+

--- services/Notifier.php
+++ services/Notifier.php
@@ -5,11 +5,18 @@ use Yii;
 
 class Notifier implements NotifierInterface
 {
+    private $fromEmail;
+
+    public function __construct($fromEmail)
+    {
+        $this->fromEmail = $fromEmail;
+    }
+
     public function notify($email, $message)
     {
         if ($email) {
             Yii::$app->mailer->compose()
-                ->setFrom(Yii::$app->params['adminEmail'])
+                ->setFrom($this->fromEmail)
                 ->setTo($email)
                 ->setSubject($message)
                 ->send();
