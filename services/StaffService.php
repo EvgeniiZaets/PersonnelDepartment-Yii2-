111815724624216997076dd448c539b3dde8d21f
EvgeniiZaets <evgeniizaets@gmail.com>
2018-07-29 13:06:45 +0300

    added events

--- services/StaffService.php
+++ services/StaffService.php
@@ -5,17 +5,76 @@ namespace app\services;
 use app\models\Interview;
 use app\repositories\InterviewRepositoryInterface;
 
+class Event
+{
+
+}
+
+class InterviewJoinEvent extends Event
+{
+    public $interview;
+
+    public function __construct(Interview $interview)
+    {
+        $this->interview = $interview;
+    }
+}
+
+class InterviewJoinEventListener
+{
+    public function handle(InterviewJoinEvent $event)
+    {
+        echo $event->interview->id;
+    }
+}
+
+class EventDispatcher
+{
+    private $listeners = []; // содержит обработчики событий
+
+    public function __construct(array $listeners)
+    {
+        $this->listeners = $listeners;
+    }
+
+    // Ищет по имени класса $event обработчики в массиве $this->listeners и вызывет их.
+    public function dispatch(Event $event)
+    {
+        $eventName = get_class($event);
+        if (isset($this->listeners[$eventName])) {
+            foreach ($this->listeners[$eventName] as $className) {
+                $listener = \Yii::$container->get($className);
+                $listener->handle($event); // Обрабатывает $event
+            }
+        }
+    }
+}
+
+// Навещиваем обработчики на события.
+$eventDispatcher = new EventDispatcher([
+    'app\services\InterviewJoinEvent' => [
+        'app\services\InterviewJoinEventListener'
+    ]
+]);
+
 class  StaffService
 {
     private $interviewRepository;
     private $logger;
     private $notifier;
+    private $eventDispatcher; // содержит обработчики событий
 
-    public function __construct(InterviewRepositoryInterface $interviewRepository, LoggerInterface $logger, NotifierInterface $notifier)
+    public function __construct(
+        InterviewRepositoryInterface $interviewRepository,
+        LoggerInterface $logger,
+        NotifierInterface $notifier,
+        EventDispatcher $eventDispatcher
+    )
     {
         $this->interviewRepository = $interviewRepository;
         $this->logger = $logger;
         $this->notifier = $notifier;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function joinToInterview($lastName, $firstName, $email, $date)
@@ -25,6 +84,11 @@ class  StaffService
         // 2. может быть несколько способов создания одного и того же обьекта, тогда одного конструктора не хватит.
         $interview = Interview::join($lastName, $firstName, $email, $date);
         $this->interviewRepository->add($interview);
+
+        // При вызове dispatch(), диспетчер автоматически по имени класса
+        // циклом пройдет по всем привязанным к этому событию обработчикам
+        // и вызовет каждый, передаваю туда InterviewJoinEvent.
+        $this->eventDispatcher->dispatch(new InterviewJoinEvent($interview));
 
         $this->notifier->notify($interview->email, 'You are joined to interview!');
         $this->logger->log($interview->last_name . ' ' . $interview->first_name . ' is joined to interview');
